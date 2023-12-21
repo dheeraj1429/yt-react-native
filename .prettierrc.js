5a3449c2c74edf2ef83296ea22395aee3273bda9
dheeraj8585 <dheeraj@metaversitytechnologies.com>
2023-12-21 21:11:43 +0530

    first commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+   arrowParens: 'avoid',
+   bracketSameLine: true,
+   bracketSpacing: true,
+   singleQuote: true,
+   trailingComma: 'all',
+   printWidth: 150,
+   tabWidth: 3,
 };
